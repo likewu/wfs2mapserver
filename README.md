4ae50a32e6cfa509eee1a76e2333a57a5ac55bde
likewu <likewu@126.com>
2023-12-03 16:53:51 +0800

    aa

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## 
+
+1 api通过get获取客户端请求，参数包含图层、x坐标、y坐标。
+2 代理使用rust通过ffi调用libmapserver功能，实现mapserver。
+3 代理创建mapObj，转换wfs格式到栅格png返回到客户端。
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-sudo apt install libproj-dev libgdal-dev
+sudo apt install libproj-dev libgdal-dev libprotobuf-c-dev
 #sudo apt install libmapserver-dev
+sudo apt install apt-transport-https ca-certificates -y
+#sudo update-ca-certificates
 RUSTFLAGS="-Clink-arg=-L -Clink-arg=/mnt/data/app/opt/lib" cargo build
 LD_LIBRARY_PATH=/mnt/data/app/opt/lib /mnt/data/app/julia/wfs2map/target/debug/mapserver
 
@@ -10,11 +12,17 @@ LD_LIBRARY_PATH=/mnt/data/app/opt/lib /mnt/data/app/julia/wfs2map/target/debug/m
 
 https://demo.mapserver.org/cgi-bin/wfs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetCapabilities
 https://demo.mapserver.org/cgi-bin/wfs?SERVICE=WFS&VERSION=1.0.0&REQUEST=getfeature&TYPENAME=continents&MAXFEATURES=100
+https://demo.mapserver.org/cgi-bin/wfs?SERVICE=WFS&VERSION=1.0.0&REQUEST=DescribeFeatureType&TYPENAME=continents&OUTPUTFORMAT=XMLSCHEMA
 
 
-sudo apt install libcurlpp-dev libxml2-dev libprotobuf-c-dev protobuf-c-compiler libfcgi-dev swig
+sudo apt install libcurlpp-dev libxml2-dev protobuf-c-compiler libfcgi-dev swig
 sudo apt install ruby-full
 cd mapserver-8.0.1/build
-cmake -DCMAKE_INSTALL_PREFIX=/mnt/data/app/opt -DCMAKE_PREFIX_PATH=/usr/local/pgsql/91:/usr/local:/opt  -DWITH_CLIENT_WFS=ON -DWITH_CLIENT_WMS=ON -DWITH_CURL=ON -DWITH_SOS=ON -DWITH_JAVA=OFF -DWITH_CSHARP=OFF -DWITH_PERL=OFF -DWITH_RUBY=OFF -DWITH_SVGCAIRO=OFF -DWITH_ORACLESPATIAL=OFF ../ >../configure.out.txt
+cmake -DCMAKE_INSTALL_PREFIX=/mnt/data/app/opt -DCMAKE_PREFIX_PATH=/usr/local/pgsql/91:/usr/local:/opt  -DWITH_CLIENT_WFS=ON -DWITH_CLIENT_WMS=ON -DWITH_CURL=ON -DWITH_SOS=ON -DWITH_JAVA=OFF -DWITH_CSHARP=OFF -DWITH_PERL=OFF -DWITH_RUBY=OFF -DWITH_SVGCAIRO=OFF -DWITH_ORACLESPATIAL=OFF -DWITH_MSSQL=OFF ../ >../configure.out.txt
 make install
 /mnt/data/app/opt/bin/mapserv
+
+
+https://github.com/perrygeo/openlayers/blob/40f92963727d9e1bac1287ceea2395a1a99f50f7/lib/OpenLayers/Protocol/WFS.js#L30
+https://github.com/perrygeo/ol3/blob/163cc5b8730629955390a0a1051d273e5660c22f/src/ol/format/wfsformat.js#L27
+https://github.com/perrygeo/ol3/blob/163cc5b8730629955390a0a1051d273e5660c22f/examples/vector-wfs.js#L16
